984d7aa24e976d2c9bbb95015316bae560b65547
VenziVi <vlazarov86@gmail.com>
2022-12-10 14:32:27 +0200

    update moves

--- src/KnightMoves.c
+++ src/KnightMoves.c
@@ -4,87 +4,83 @@
 #include <stdio.h>
 #include <string.h>
 
+typedef struct
+{
+    int row;
+    int col;
+    char dir[20];
+} Moove;
+
+static void setDirection(board *playBoard, int currRow, int currCol, result *result,
+                         input *inputData, Moove *moove)
+{
+    if (checkIsDirectionValid(playBoard, currRow + moove->row, currCol + moove->col))
+    {
+        result->directions[result->moveCounter++] = strdup(moove->dir);
+        resetOldPosition(playBoard, currRow, currCol);
+        makeMove(playBoard, inputData->row += moove->row, inputData->col += moove->col, inputData);
+    }
+}
+
 void setKnightDirection(board *playBoard, result *result, char *direction, input *inputData)
 {
     int currRow = inputData->row;
     int currCol = inputData->col;
+    Moove moove;
 
     if (strcmp(direction, "ul") == 0)
     {
-        if (checkIsDirectionValid(playBoard, currRow - 2, currCol - 1))
-        {
-            result->directions[result->moveCounter++] = strdup("up-left");
-            resetOldPosition(playBoard, currRow, currCol);
-            makeMove(playBoard, inputData->row -= 2, inputData->col -= 1, inputData);
-        }
-        
+        moove.row = -2;
+        moove.col = -1;
+        strcpy(moove.dir, "up-left");        
     }
     else if (strcmp(direction, "ur") == 0)
     {
-        if (checkIsDirectionValid(playBoard, currRow - 2, currCol + 1))
-        {
-            result->directions[result->moveCounter++] = strdup("up-right");
-            resetOldPosition(playBoard, currRow, currCol);
-            makeMove(playBoard, inputData->row -= 2, inputData->col += 1, inputData);
-        }
+        moove.row = -2;
+        moove.col = 1;
+        strcpy(moove.dir, "up-right");
     }
     else if (strcmp(direction, "ru") == 0)
     {
-        if (checkIsDirectionValid(playBoard, currRow - 1, currCol + 2))
-        {
-            result->directions[result->moveCounter++] = strdup("right-up");
-            resetOldPosition(playBoard, currRow, currCol);
-            makeMove(playBoard, inputData->row -= 1, inputData->col += 2, inputData);
-        }
+        moove.row = -1;
+        moove.col = 2;
+        strcpy(moove.dir, "right-up");
     }
     else if (strcmp(direction, "rd") == 0)
     {
-        if (checkIsDirectionValid(playBoard, currRow + 1, currCol + 2))
-        {
-            result->directions[result->moveCounter++] = strdup("right-down");
-            resetOldPosition(playBoard, currRow, currCol);
-            makeMove(playBoard, inputData->row += 1, inputData->col += 2, inputData);
-        }
+        moove.row = 1;
+        moove.col = 2;
+        strcpy(moove.dir, "right-down");
     }
     else if (strcmp(direction, "dl") == 0)
     {
-        if (checkIsDirectionValid(playBoard, currRow + 2, currCol - 1))
-        {
-            result->directions[result->moveCounter++] = strdup("down-left");
-            resetOldPosition(playBoard, currRow, currCol);
-            makeMove(playBoard, inputData->row += 2, inputData->col -= 1, inputData);
-        }
+        moove.row = 2;
+        moove.col = -1;
+        strcpy(moove.dir, "down-left");
     }
     else if (strcmp(direction, "dr") == 0)
     {
-        if (checkIsDirectionValid(playBoard, currRow + 2, currCol + 1))
-        {
-            result->directions[result->moveCounter++] = strdup("down-right");
-            resetOldPosition(playBoard, currRow, currCol);
-            makeMove(playBoard, inputData->row += 2, inputData->col += 1, inputData);
-        }
+        moove.row = 2;
+        moove.col = 1;
+        strcpy(moove.dir, "down-right");
     }
     else if (strcmp(direction, "lu") == 0)
     {
-        if (checkIsDirectionValid(playBoard, currRow - 1, currCol - 2))
-        {
-            result->directions[result->moveCounter++] = strdup("left-up");
-            resetOldPosition(playBoard, currRow, currCol);
-            makeMove(playBoard, inputData->row -= 1, inputData->col -= 2, inputData);
-        }
+        moove.row = -1;
+        moove.col = -2;
+        strcpy(moove.dir, "left-up");
     }
     else if (strcmp(direction, "ld") == 0)
-    {
-        if (checkIsDirectionValid(playBoard, currRow + 1, currCol - 2))
-        {
-            result->directions[result->moveCounter++] = strdup("left-down");
-            resetOldPosition(playBoard, currRow, currCol);
-            makeMove(playBoard, inputData->row += 1, inputData->col -= 2, inputData);
-        }
+    {   
+        moove.row = 1;
+        moove.col = -2;
+        strcpy(moove.dir, "left-down");
     }
     else
     {
         printf("INVALID DIRECTION!\n");
+        return;
     }
     
+    setDirection(playBoard, currRow, currCol, result, inputData, &moove);
 }
